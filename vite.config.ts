54ed264530abb28ea9c8169e2ab390a5a0efb030
mlucas4330 <mlucas4330@gmail.com>
2026-03-02 16:03:31 -0300

    build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/teste/',
+  base: '/energia/',
   plugins: [react(), tailwindcss()],
 })
